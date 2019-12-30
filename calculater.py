144ab77e19f877ed3a681d956e7b7ff6146d4793
MaiFujisawa <57941202+MaiFujisawa@users.noreply.github.com>
2019-12-30 15:51:02 +0900

    Update calculater.py

--- calculater.py
+++ calculater.py
@@ -15,7 +15,7 @@ def multiple(str1,str2,num2calc = {}):
     return num2calc
 
 
-def cut(str1,str2,num2calc = {}):
+def divide(str1,str2,num2calc = {}):
     num2calc[str1,str2] = str1/str2
     return num2calc
 
@@ -25,14 +25,12 @@ def main():
     second_number = input('-->')
     first_number = float(first_number)
     second_number = float(second_number)
-    #first_number and second_number is str
-
-    #list->dictionary
-    addition = plus(first_number,second_number)
-    subtraction = minus(first_number,second_number)
-    multiplication = multiple(first_number,second_number)
-    division = cut(first_number,second_number)
-    print(division)
+    
+    
+    print(plus(first_number,second_number))
+    print(minus(first_number,second_number))
+    print(multiple(first_number,second_number))
+    print(divide(first_number,second_number))
 
 
 if __name__ == '__main__':
